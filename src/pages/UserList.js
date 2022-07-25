de157bd62ae25cbd6969bbf45cf514fef36c1642
haykelmhadhbi <mhadhbi.haikel@gmail.com>
2022-07-25 11:13:28 +0100

    deploy my react app movies to github pages

---
+++ src/pages/UserList.js
@@ -0,0 +1,42 @@
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import Card from "../components/Card";
+
+import Header from "../components/Header";
+
+const UserList = () => {
+  const [listData, setListData] = useState ([]);
+  useEffect(() => {
+    let moviesId = window.localStorage.movies
+      ? window.localStorage.movies.split(",")
+      : [];
+      for (let i = 0; i < moviesId.length; i++) {
+            axios.get(
+           `https://api.themoviedb.org/3/movie/${moviesId[i]}?api_key=ed82f4c18f2964e75117c2dc65e2161d&language=fr-FR`
+         ).then((res) => setListData((listData) => [...listData, res.data]));
+        }
+               }, 
+           []);
+
+
+
+
+
+    return(
+      <div className="user-list-page">
+        <Header/>
+        Coup de Coeur <span>💖</span>
+
+      <div className="result">
+        {listData.length > 0 ?(
+          listData.map((movie) => <Card movie={movie} key={movie.id}/>)
+        ) : (
+          <h2>Aucun coup de coeur pour le moment</h2>
+          )}
+
+      </div>
+</div>
+    );
+    
+    };
+    export default UserList ;
